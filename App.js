0b4e5565f15b88b6bddc5511f6db793e8c449640
Chibueze Kalu <kalu.chibueze@hotmail.com>
2020-11-21 08:52:04 +0100

    Added my name

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is Chibueze</Text>
       <StatusBar style="auto" />
     </View>
   );
